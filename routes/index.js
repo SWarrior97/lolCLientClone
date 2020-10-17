889af1470afd28e8ab92914077002479cae83d8e
SWarrior97 <carlosgomes1997.edu@gmail.com>
2020-10-17 01:58:03 +0100

    register and login done. TODO add JWT to login

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 var path = require("path");
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 
 // @desc   
@@ -16,12 +19,26 @@ router.get('/',async (req,res) =>{
 	}
 })
 
-router.get('/login',async (req,res) =>{
+router.post('/login',async (req,res) =>{
 	try{
+		let user = await User.findOne({username:req.body.username}).exec();
+		
 
-		res.render('dashboard',{
-            layout:'main.hbs'
-        })
+		if(user == null){
+			res.render('error/custom',{
+				title:"Wrong Credential",
+				content:"Your login credentials don't match an account in our system"
+			})
+		}else{
+			if (bcrypt.compareSync(req.body.password, user.password)) {
+				console.log("true")
+			} else {
+				res.render('error/custom',{
+					title:"Wrong Credential",
+					content:"Your login credentials don't match an account in our system"
+				})
+			}
+		}
 
 	}catch(err){
 		console.log(err)
@@ -29,5 +46,33 @@ router.get('/login',async (req,res) =>{
 	}
 })
 
+router.get('/register',async (req,res) =>{
+	try{
+		res.render('register', {
+			layout: 'login',
+		})
+	}catch(err){
+		console.log(err)
+		res.render('error/500')
+	}
+})
+
+router.post('/register',async (req,res) =>{
+	try{
+		if(req.body.password == "" || req.body.username == ""){
+			res.render('error/500')
+		}else{
+			bcrypt.hash(req.body.password, saltRounds, async function (err,   hash) {
+				req.body.password = hash;
+				console.log(req.body)
+				await User.create(req.body)
+				res.redirect('/')
+			});
+		}
+	}catch(err){
+		console.log(err)
+		res.render('error/500')
+	}
+})
 
 module.exports = router
